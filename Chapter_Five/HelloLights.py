d71527a70478c11f3f8851cee557b47adcd827d3
electrosy <heysteve5@gmail.com>
2025-01-03 20:34:34 -0800

    rename verticies

--- Chapter_Five/HelloLights.py
+++ Chapter_Five/HelloLights.py
@@ -6,33 +6,57 @@ from Mesh3D import *
 
 class Cube(Mesh3D):
   def __init__(self, draw_type, filename):
-    self.vertices = [[-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, -0.5],
-                        [-0.5, -0.5, 0.5], [0.5, -0.5, 0.5], [0.5, 0.5, 0.5], [-0.5, 0.5, 0.5]]
+      A = (-0.5, -0.5, -0.5)
+      B = (0.5, -0.5, -0.5)
+      C = (0.5, 0.5, -0.5)
+      D = (-0.5, 0.5, -0.5)
+      E = (-0.5, -0.5, 0.5)
+      F = (0.5, -0.5, 0.5)
+      G = (0.5, 0.5, 0.5)
+      H = (-0.5, 0.5, 0.5)
 
-    self.triangles = [ 0, 1, 2, 0, 2, 3, #Front face:
-                        4, 5, 6, 4, 6, 7, #Back face:
-                        4, 0, 3, 4, 3, 7, #Left face:
-                        1, 5, 6, 1, 6, 2, #Right face:
-                        3, 2, 6, 3, 6, 7, #Top face:
-                        4, 5, 1, 4, 1, 0] #Bottom face: #42
+      self.vertices = [F, E, G, H,
+                       C, D, B, A,
+                       G, H, C, D,
+                       B, F, E, A,
+                       E, H, D, A,
+                       B, C, G, F
+                       ]
 
-    self.uvs = [ # Front face (XY plane, no changes needed)
-        # Front face
-        (0, 0), (1, 0), (1, 1), (0, 1),
-        # Back face
-        (0, 0), (1, 0), (1, 1), (0, 1),
-        # Left face
-        (0, 0), (1, 0), (1, 1), (0, 1),
-        # Right face
-        (0, 0), (1, 0), (1, 1), (0, 1),
-        # Top face
-        (0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0),
-        # Bottom face
-        (0, 0), (1, 0), (1, 1), (0, 1)]
+      self.triangles = [0, 2, 3, 0, 3, 1,
+                        8, 4, 5, 8, 5, 9,
+                        10, 6, 7, 10, 7, 11,
+                        12, 13, 14, 12, 14, 15,
+                        16, 17, 18, 16, 18, 19,
+                        20, 21, 22, 20, 22, 23]
 
-    Mesh3D.texture = pygame.image.load(filename)
-    Mesh3D.draw_type = draw_type
-    Mesh3D.init_texture(self)
+      self.uvs = [(0.0, 0.0),
+                  (1.0, 0.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (0.0, 0.0),
+                  (1.0, 0.0),
+                  (0.0, 0.0),
+                  (1.0, 0.0),
+                  (0.0, 0.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (1.0, 0.0),
+                  (0.0, 0.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (1.0, 0.0),
+                  (0.0, 0.0),
+                  (0.0, 1.0),
+                  (1.0, 1.0),
+                  (1.0, 0.0)]
+      Mesh3D.texture = pygame.image.load(filename)
+      Mesh3D.draw_type = draw_type
+      Mesh3D.init_texture(self)
 
 
 pygame.init()
