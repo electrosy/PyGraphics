34bec558419b76269d3b491d31d2020932f68efd
electrosy <heysteve5@gmail.com>
2025-01-04 09:22:22 -0800

    Updated verticies and uvs to make the cube work properly

--- Chapter_Five/HelloLights.py
+++ Chapter_Five/HelloLights.py
@@ -15,45 +15,28 @@ class Cube(Mesh3D):
       G = (0.5, 0.5, 0.5)
       H = (-0.5, 0.5, 0.5)
 
-      self.vertices = [F, E, G, H,
-                       C, D, B, A,
-                       G, H, C, D,
-                       B, F, E, A,
-                       E, H, D, A,
-                       B, C, G, F
+      self.vertices = [A, B, C, D, #0, 1, 2, 3 - front
+                       F, E, H, G, #4, 5, 6, 7 - back
+                       B, F, G, C, #8,9,10,11 - left
+                       E, A, D, H, #12,13,14,15 - right
+                       D, C, G, H, #16,17,18,19 - top
+                       E, F, B, A  #20,21,22,23 - bottom
                        ]
 
-      self.triangles = [0, 2, 3, 0, 3, 1,
-                        8, 4, 5, 8, 5, 9,
-                        10, 6, 7, 10, 7, 11,
+      self.triangles = [0, 1, 2, 0, 2, 3,
+                        4, 5, 6, 4, 6, 7,
+                        8, 9, 10, 8, 10, 11,
                         12, 13, 14, 12, 14, 15,
                         16, 17, 18, 16, 18, 19,
                         20, 21, 22, 20, 22, 23]
 
-      self.uvs = [(0.0, 0.0),
-                  (1.0, 0.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (0.0, 0.0),
-                  (1.0, 0.0),
-                  (0.0, 0.0),
-                  (1.0, 0.0),
-                  (0.0, 0.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (1.0, 0.0),
-                  (0.0, 0.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (1.0, 0.0),
-                  (0.0, 0.0),
-                  (0.0, 1.0),
-                  (1.0, 1.0),
-                  (1.0, 0.0)]
+      self.uvs = [(0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0),
+                  (0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0),
+                  (0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0),
+                  (0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0),
+                  (0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0),
+                  (0.0, 0.0),(1.0, 0.0),(1.0, 1.0),(0.0, 1.0)]
+
       Mesh3D.texture = pygame.image.load(filename)
       Mesh3D.draw_type = draw_type
       Mesh3D.init_texture(self)
@@ -67,8 +50,8 @@ pygame.display.set_caption('OpenGL in Python')
 done = False
 white = pygame.Color(255, 255, 255)
 glMatrixMode(GL_PROJECTION)
-#gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
-glOrtho(-1, 1, 1, -1, 0.1, 100.0)
+gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
+#glOrtho(-1, 1, 1, -1, 0.1, 100.0)
 glMatrixMode(GL_MODELVIEW)
 translate = -2
 translate_to = 0
@@ -95,8 +78,9 @@ glMaterialfv(GL_FRONT, GL_DIFFUSE, (0, 1, 0, 1))
 
 #mesh = Mesh3D()
 cube = Cube(GL_POLYGON, "brick-wall.jpg")
-
+auto_rotate = False
 while not done:
+
   for event in pygame.event.get():
     if event.type == pygame.MOUSEWHEEL:
         print(event.x, event.y)
@@ -123,14 +107,20 @@ while not done:
         if event.key == pygame.K_DOWN:
             rotation = rotation_x = 1
             rotation_angle = rotation_angle_step
+        if event.key == pygame.K_SPACE:
+            if auto_rotate:
+                auto_rotate = False
+            else:
+                auto_rotate = True
         glRotatef(rotation_angle, rotation_x, rotation_y, 0)
     if event.type == pygame.QUIT:
       done = True
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-  #glRotatef(5, 1, 0, 1)
+  if auto_rotate:
+    glRotatef(1.25, 1, 0, 1)
 
   #mesh.draw()
   cube.draw()
   pygame.display.flip()
-  pygame.time.wait(100);
+  pygame.time.wait(25);
 pygame.quit()
